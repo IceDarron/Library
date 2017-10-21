bb3c9592514db737e6b7fec5289b12593d5f8279
IceDarron <darron_r@163.com>
2017-10-22 01:59:34 +0800

    增加常量类

---
+++ src/com/iceDarron/core/utils/ConstantUtils.java
@@ -0,0 +1,13 @@
+package com.iceDarron.core.utils;
+
+public class ConstantUtils {
+
+    /**
+     * 缓存静态变量
+     */
+    // 统一分隔符
+    public final static String SEPARATIVE_SIGN = "/|";
+
+    // codeType 代码类型
+    public final static String HKEY_CODE_TYPE = "HKEY_CODE_TYPE";
+}
